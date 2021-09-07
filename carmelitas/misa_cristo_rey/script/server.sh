bf20b4ae282271e1d4319e4dc03bdb335bfcf654
searchsam <samgutiaviles@yahoo.es>
2021-09-07 12:58:35 -0600

    Incluir server

---
+++ carmelitas/misa_cristo_rey/script/server.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+# script/server: Launch the application and any extra required processes locally.
+
+set -e
+
+cd "$(dirname "$0")/.."
+
+# ensure everything in the app is up to date.
+# sh script/update.sh
+
+# test -z "$RACK_ENV" && RACK_ENV='development'
+
+# boot the app and any other necessary processes.
+echo "==> Compiling PDF book..."
+
+compile_pdf() {
+    cd libro
+
+    readarray -d . -t strarr <<<"$1"
+    name="${strarr[0]}"}
+
+    rm -rf out/
+
+    lilypond-book --output=out --pdf $1
+
+    cd out
+
+    pdflatex "${name}.tex"
+
+    cd ..
+
+    cp "out/${name}.pdf" .
+
+    rm -rf out/
+
+    cd ..
+}
+
+echo "==> Compiling audio book..."
+
+compile_audio() {
+    names=(agnus_dei aleluya communio credo finalis gloria_in_excelsis_deo graduale introito kyrie_eleison offertorium sanctus)
+    compile_dir=$1/*.ly
+    for file in $compile_dir; do
+        readarray -d / -t temp <<<"${file}"
+        file_name="${temp[-1]}"
+
+        readarray -d . -t strarr <<<"$file_name"
+        name="${strarr[0]}"
+
+        if [[ ! " ${names[*]} " =~ " ${name} " ]]; then
+            if [ -f "audio/${name}.mp3" ]; then
+                rm "audio/${name}.mp3"
+            fi
+
+            lilypond -dbackend=null $file
+
+            if [ -f "${name}.midi" ]; then
+                timidity "${name}.midi" -Ow -o - | ffmpeg -i - -acodec libmp3lame -ab 64k "audio/${name}.mp3"
+                rm "${name}.midi"
+            fi
+        fi
+    done
+}
+
+compile_dir=libro/*.lytex
+files=($compile_dir)
+
+if [ ${#files[@]} -gt 1 ]; then
+    for file in $compile_dir; do
+        readarray -d / -t strarr <<<"$file"
+        file_name="${strarr[-1]}"
+
+        compile_pdf $file_name
+        if [[ $file_name == *"moderna"* ]]; then
+            compile_audio "ordinario"
+            compile_audio "propio"
+        fi
+    done
+fi
